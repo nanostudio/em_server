8bf411322836dbd217aa98a1a72a73f40d2d8aed
Dimas Cyriaco <dimascyriaco@gmail.com>
2011-06-24 21:11:18 -0300

    Transformando chat em jogo multiplayer

--- Gemfile
+++ Gemfile
@@ -3,4 +3,7 @@ source :rubygems
 gem 'eventmachine', '1.0.0.beta.3'
 gem 'em-http-request', '1.0.0.beta.4  '
 gem 'em-websocket'
+gem 'json'
 gem 'guard'
+
+gem 'ruby-debug19'
--- lib/server.rb
+++ lib/server.rb
@@ -5,30 +5,50 @@ require "bundler/setup"
 require 'eventmachine'
 require 'em-websocket'
 require 'em-http'
+require 'ruby-debug'
+require 'json'
 
-@@clients = Array.new
+
+class Player
+  @@all = Array.new
+  @@count = 0
+
+  attr_accessor :name, :uid, :ws
+
+  def initialize(ws)
+    @uid = rand(10000)
+    @ws = ws
+    @@count += 1
+    @name = "player#{@@count}"
+    @@all << {@id => @ws}
+    ws.send({ :action => :player_info, :params => { :id => @uid, :name => @name} }.to_json)
+  end
+
+  def self.all
+    @@all
+  end
+end
 
 EM.run do
   EM::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
-    # channel = EM::Channel.new
-
     ws.onopen do
       puts "Client connected."
-      @@clients << ws
-      # channel.subscribe{ |msg| p [:got, 'msg'] }
+      player = Player.new(ws)
+      Player.all.each { |c| player.ws.send({:action => 'new_player_info', :params => {:id => player.uid, :name => player.name}}.to_json) }
     end
 
     ws.onmessage do |data|
-      puts "Message received: '#{data.strip}'."
-      @@clients.each { |c| c.send(data.strip) }
+      puts "Message received: '#{JSON.parse(data)}'."
+      data = JSON.parse(data)
+      Player.all.each { |c| c[:ws].send({:state => data[data.keys.first]}.merge(:name => data.keys.first).to_json) }
     end
 
     ws.onclose do
-      @@clients.delete(self)
+      # @@clients.delete(self)
       puts "Client disconnected."
     end
-    ws.onerror { |e| puts "err #{e.message}" }
-  end
 
-  puts "Server is up! Open 'index.html' on your browser."
+    ws.onerror { |e| puts "err #{e.message}\n#{caller.join("\n")}" }
+  end
+  puts "Server is up! Open '#{File.expand_path('../../index.html', __FILE__)}' on your browser."
 end
